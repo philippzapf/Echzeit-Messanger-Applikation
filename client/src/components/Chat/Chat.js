986a9ecf00061c344f3958d59525eaaad39ab55e
philippzapf <p-zapf@web.de>
2020-05-28 15:58:57 +0200

    Setup der Eingabefelder

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from "socket.io-client";
 
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 let socket;
 
@@ -51,7 +52,8 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room}/>
-          <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}/>
+
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
   );
