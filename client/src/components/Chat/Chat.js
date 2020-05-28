d3e89591469fbb316f12f30cc0e51b09e445407a
philippzapf <p-zapf@web.de>
2020-05-28 15:46:52 +0200

    Setup der Chatbox mit CSS-Style

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
 
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+
 let socket;
+
+
 
 
 const Chat = ({ location }) => {
@@ -45,6 +50,7 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
+        <InfoBar room={room}/>
           <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}/>
       </div>
     </div>
