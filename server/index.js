39e8359c12674043608e14e5dd4d5fffd3b276c6
philippzapf <p-zapf@web.de>
2020-05-28 14:58:11 +0200

    Basic Setup der Login-Daten

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +12,10 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
     console.log('We have a new connection.');
+
+    socket.on('join', ({name, room}, callback) => {
+        console.log(name, room);
+    });
 
     socket.on('disconnect', () => {
         console.log('User had left!!!');
